7d08b8a6af1fd198b3bd388825bbd52c52ffada6
agrawalshub <141906410+agrawalshub@users.noreply.github.com>
2024-11-28 17:11:05 +0000

    variable and datatype

---
+++ basic/data_type.js
@@ -0,0 +1,10 @@
+/*
+data type in js
+number => 12345
+boolean => true or false
+bigInt
+string => "" 
+null => standalone value
+undifine => empty value
+
+*/
---
+++ basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountName = "shubham"
+var accountEmail ="shubham@gmail.com"
+accountCity = "kanpur"
+
+// const accountId = 34567; const value can't be change
+
+accountName = "vijay"
+accountEmail = "vif@google.com"
+accountCity = "delhi"
+console.log(accountId);
+
+console.table([accountName ,accountEmail, accountCity])
