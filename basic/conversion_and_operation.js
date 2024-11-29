e50f3a46edc3686b6c84d4bf942c80a5c56b9721
agrawalshub <141906410+agrawalshub@users.noreply.github.com>
2024-11-29 04:22:50 +0000

    conversionOperation

---
+++ basic/conversion_and_operation.js
@@ -0,0 +1,23 @@
+let num = "33"
+
+console.log (num)
+
+console.log(typeof num);
+
+let changeNumber = Number(num)
+console.log(typeof changeNumber);
+
+/*
+ 33 => number 
+ 33adcc => NaN
+ true => 1 , false => 0
+ null => 0
+ undifine => NaN
+*/
+
+let userLogIn = 1
+
+let booleanUser = Boolean(userLogIn)
+
+console.log(booleanUser);
+
