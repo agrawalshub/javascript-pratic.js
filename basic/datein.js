8f66dee5304aa292710820d47c630b2a1de283a3
agrawalshub <141906410+agrawalshub@users.noreply.github.com>
2024-12-23 11:52:29 +0000

    date

---
+++ basic/datein.js
@@ -0,0 +1,13 @@
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(typeof mydate);
+
+
+let myNewdate
+
+
+
